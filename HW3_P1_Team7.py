b7018db869ff55c9cde0d460f6b585dedafeeb69
ewilson294 <ecwil2@yahoo.com>
2021-09-23 10:52:44 -0500

    Initial Commit

---
+++ HW3_P1_Team7.py
@@ -0,0 +1,9 @@
+""" 
+Homework 3
+Problem 1
+Team#: 7
+Team Member-1: Eric Wilson - Member’s Contribution 33%
+Team Member-2: Ha Tran - Member’s Contribution 33%
+Team Member-3: Quynh Tran - Member’s Contribution 33%
+"""
+
