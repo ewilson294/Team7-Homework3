65f1af5f5892c1dacb11b33494b888aca7cccb8a
ewilson294 <ecwil2@yahoo.com>
2021-09-25 18:05:51 -0500

    Output almost matches example, final row has discrepancies.

--- HW3_P2_Team7.py
+++ HW3_P2_Team7.py
@@ -16,18 +16,22 @@ table_header = 'Month Average  Minimum Maximum'
 print(table_header)
 print('=' * len(table_header))
 # Gather Statistics for steps by month
+month_code = 0 # separate index for month of the year
 for month in calendar_days:
-    daily_steps = 0
-    minimum = None
-    maximum = None
+    month_code += 1 # increase by 1 for each month
+    daily_steps = 0 # Initial condition for the amount of steps in the month
+    minimum = None # Initial condition for the minimum
+    maximum = None # Initial condition for the maximum
     for day in range(month):
         ds = steps.readline() # Hold the line as a string before converting
-        ds = float(ds[:-1])
+        ds = float(ds[:-1]) # Convert the string into a float
         if maximum == None or maximum < ds:
-            maximum = ds
+            maximum = ds # keep the maximum amount of steps for the month
         if minimum == None or minimum > ds:
-            minimum = ds
-        daily_steps += ds
-    average = daily_steps/month
-    print(month, format(average, '4.2f'), format(minimum, '4.0f'), format(maximum, '5.0f'), sep=' ')
+            minimum = ds # keep the minimum amount of steps for the month
+        daily_steps += ds # keep a running total for the amount of steps in the month
+    average = daily_steps/month # Calculate average amount of steps in a month
+    # print a row of the table of summary statistics for every month
+    print(format(month_code, '2d'), ' '*4, format(average, '7.2f'), ' '*5, format(minimum, '4.0f'), ' '*3, format(maximum, '5.0f'), sep='')
+# Close steps.txt
 steps.close()
